b20e395a661b075f7b90adcdb7abde0fca4914d5
unknown <463003@itri.org.tw>
2024-06-24 10:25:59 +0800

    a

---
+++ pointcept/datasets/preprocessing/nuscenes/preprocess_nuscenes_info_test.py
@@ -0,0 +1,396 @@
+import os
+from pathlib import Path
+import numpy as np
+import argparse
+import tqdm
+import pickle
+from functools import reduce
+from pyquaternion import Quaternion
+from nuscenes.nuscenes import NuScenes
+from nuscenes.utils import splits
+from nuscenes.utils.geometry_utils import transform_matrix
+
+# Keep the necessary helper functions: get_available_scenes, get_sample_data, quaternion_yaw, obtain_sensor2top
+map_name_from_general_to_detection = {
+    "human.pedestrian.adult": "pedestrian",
+    "human.pedestrian.child": "pedestrian",
+    "human.pedestrian.wheelchair": "ignore",
+    "human.pedestrian.stroller": "ignore",
+    "human.pedestrian.personal_mobility": "ignore",
+    "human.pedestrian.police_officer": "pedestrian",
+    "human.pedestrian.construction_worker": "pedestrian",
+    "animal": "ignore",
+    "vehicle.car": "car",
+    "vehicle.motorcycle": "motorcycle",
+    "vehicle.bicycle": "bicycle",
+    "vehicle.bus.bendy": "bus",
+    "vehicle.bus.rigid": "bus",
+    "vehicle.truck": "truck",
+    "vehicle.construction": "construction_vehicle",
+    "vehicle.emergency.ambulance": "ignore",
+    "vehicle.emergency.police": "ignore",
+    "vehicle.trailer": "trailer",
+    "movable_object.barrier": "barrier",
+    "movable_object.trafficcone": "traffic_cone",
+    "movable_object.pushable_pullable": "ignore",
+    "movable_object.debris": "ignore",
+    "static_object.bicycle_rack": "ignore",
+}
+
+
+cls_attr_dist = {
+    "barrier": {
+        "cycle.with_rider": 0,
+        "cycle.without_rider": 0,
+        "pedestrian.moving": 0,
+        "pedestrian.sitting_lying_down": 0,
+        "pedestrian.standing": 0,
+        "vehicle.moving": 0,
+        "vehicle.parked": 0,
+        "vehicle.stopped": 0,
+    },
+    "bicycle": {
+        "cycle.with_rider": 2791,
+        "cycle.without_rider": 8946,
+        "pedestrian.moving": 0,
+        "pedestrian.sitting_lying_down": 0,
+        "pedestrian.standing": 0,
+        "vehicle.moving": 0,
+        "vehicle.parked": 0,
+        "vehicle.stopped": 0,
+    },
+    "bus": {
+        "cycle.with_rider": 0,
+        "cycle.without_rider": 0,
+        "pedestrian.moving": 0,
+        "pedestrian.sitting_lying_down": 0,
+        "pedestrian.standing": 0,
+        "vehicle.moving": 9092,
+        "vehicle.parked": 3294,
+        "vehicle.stopped": 3881,
+    },
+    "car": {
+        "cycle.with_rider": 0,
+        "cycle.without_rider": 0,
+        "pedestrian.moving": 0,
+        "pedestrian.sitting_lying_down": 0,
+        "pedestrian.standing": 0,
+        "vehicle.moving": 114304,
+        "vehicle.parked": 330133,
+        "vehicle.stopped": 46898,
+    },
+    "construction_vehicle": {
+        "cycle.with_rider": 0,
+        "cycle.without_rider": 0,
+        "pedestrian.moving": 0,
+        "pedestrian.sitting_lying_down": 0,
+        "pedestrian.standing": 0,
+        "vehicle.moving": 882,
+        "vehicle.parked": 11549,
+        "vehicle.stopped": 2102,
+    },
+    "ignore": {
+        "cycle.with_rider": 307,
+        "cycle.without_rider": 73,
+        "pedestrian.moving": 0,
+        "pedestrian.sitting_lying_down": 0,
+        "pedestrian.standing": 0,
+        "vehicle.moving": 165,
+        "vehicle.parked": 400,
+        "vehicle.stopped": 102,
+    },
+    "motorcycle": {
+        "cycle.with_rider": 4233,
+        "cycle.without_rider": 8326,
+        "pedestrian.moving": 0,
+        "pedestrian.sitting_lying_down": 0,
+        "pedestrian.standing": 0,
+        "vehicle.moving": 0,
+        "vehicle.parked": 0,
+        "vehicle.stopped": 0,
+    },
+    "pedestrian": {
+        "cycle.with_rider": 0,
+        "cycle.without_rider": 0,
+        "pedestrian.moving": 157444,
+        "pedestrian.sitting_lying_down": 13939,
+        "pedestrian.standing": 46530,
+        "vehicle.moving": 0,
+        "vehicle.parked": 0,
+        "vehicle.stopped": 0,
+    },
+    "traffic_cone": {
+        "cycle.with_rider": 0,
+        "cycle.without_rider": 0,
+        "pedestrian.moving": 0,
+        "pedestrian.sitting_lying_down": 0,
+        "pedestrian.standing": 0,
+        "vehicle.moving": 0,
+        "vehicle.parked": 0,
+        "vehicle.stopped": 0,
+    },
+    "trailer": {
+        "cycle.with_rider": 0,
+        "cycle.without_rider": 0,
+        "pedestrian.moving": 0,
+        "pedestrian.sitting_lying_down": 0,
+        "pedestrian.standing": 0,
+        "vehicle.moving": 3421,
+        "vehicle.parked": 19224,
+        "vehicle.stopped": 1895,
+    },
+    "truck": {
+        "cycle.with_rider": 0,
+        "cycle.without_rider": 0,
+        "pedestrian.moving": 0,
+        "pedestrian.sitting_lying_down": 0,
+        "pedestrian.standing": 0,
+        "vehicle.moving": 21339,
+        "vehicle.parked": 55626,
+        "vehicle.stopped": 11097,
+    },
+}
+
+
+def get_available_scenes(nusc):
+    available_scenes = []
+    for scene in nusc.scene:
+        scene_token = scene["token"]
+        scene_rec = nusc.get("scene", scene_token)
+        sample_rec = nusc.get("sample", scene_rec["first_sample_token"])
+        sd_rec = nusc.get("sample_data", sample_rec["data"]["LIDAR_TOP"])
+        has_more_frames = True
+        scene_not_exist = False
+        while has_more_frames:
+            lidar_path, boxes, _ = nusc.get_sample_data(sd_rec["token"])
+            if not Path(lidar_path).exists():
+                scene_not_exist = True
+                break
+            else:
+                break
+        if scene_not_exist:
+            continue
+        available_scenes.append(scene)
+    return available_scenes
+
+
+def get_sample_data(nusc, sample_data_token, selected_anntokens=None):
+    """
+    Returns the data path as well as all annotations related to that sample_data.
+    Note that the boxes are transformed into the current sensor"s coordinate frame.
+    Args:
+        nusc:
+        sample_data_token: Sample_data token.
+        selected_anntokens: If provided only return the selected annotation.
+
+    Returns:
+
+    """
+    # Retrieve sensor & pose records
+    sd_record = nusc.get("sample_data", sample_data_token)
+    cs_record = nusc.get("calibrated_sensor", sd_record["calibrated_sensor_token"])
+    sensor_record = nusc.get("sensor", cs_record["sensor_token"])
+    pose_record = nusc.get("ego_pose", sd_record["ego_pose_token"])
+
+    data_path = nusc.get_sample_data_path(sample_data_token)
+
+    if sensor_record["modality"] == "camera":
+        cam_intrinsic = np.array(cs_record["camera_intrinsic"])
+    else:
+        cam_intrinsic = None
+
+    # Retrieve all sample annotations and map to sensor coordinate system.
+    if selected_anntokens is not None:
+        boxes = list(map(nusc.get_box, selected_anntokens))
+    else:
+        boxes = nusc.get_boxes(sample_data_token)
+
+    # Make list of Box objects including coord system transforms.
+    box_list = []
+    for box in boxes:
+        box.velocity = nusc.box_velocity(box.token)
+        # Move box to ego vehicle coord system
+        box.translate(-np.array(pose_record["translation"]))
+        box.rotate(Quaternion(pose_record["rotation"]).inverse)
+
+        #  Move box to sensor coord system
+        box.translate(-np.array(cs_record["translation"]))
+        box.rotate(Quaternion(cs_record["rotation"]).inverse)
+
+        box_list.append(box)
+
+    return data_path, box_list, cam_intrinsic
+
+
+def quaternion_yaw(q: Quaternion) -> float:
+    """
+    Calculate the yaw angle from a quaternion.
+    Note that this only works for a quaternion that represents a box in lidar or global coordinate frame.
+    It does not work for a box in the camera frame.
+    :param q: Quaternion of interest.
+    :return: Yaw angle in radians.
+    """
+
+    # Project into xy plane.
+    v = np.dot(q.rotation_matrix, np.array([1, 0, 0]))
+
+    # Measure yaw using arctan.
+    yaw = np.arctan2(v[1], v[0])
+
+    return yaw
+
+
+def obtain_sensor2top(
+    nusc, sensor_token, l2e_t, l2e_r_mat, e2g_t, e2g_r_mat, sensor_type="lidar"
+):
+    """Obtain the info with RT matric from general sensor to Top LiDAR.
+
+    Args:
+        nusc (class): Dataset class in the nuScenes dataset.
+        sensor_token (str): Sample data token corresponding to the
+            specific sensor type.
+        l2e_t (np.ndarray): Translation from lidar to ego in shape (1, 3).
+        l2e_r_mat (np.ndarray): Rotation matrix from lidar to ego
+            in shape (3, 3).
+        e2g_t (np.ndarray): Translation from ego to global in shape (1, 3).
+        e2g_r_mat (np.ndarray): Rotation matrix from ego to global
+            in shape (3, 3).
+        sensor_type (str): Sensor to calibrate. Default: "lidar".
+
+    Returns:
+        sweep (dict): Sweep information after transformation.
+    """
+    sd_rec = nusc.get("sample_data", sensor_token)
+    cs_record = nusc.get("calibrated_sensor", sd_rec["calibrated_sensor_token"])
+    pose_record = nusc.get("ego_pose", sd_rec["ego_pose_token"])
+    data_path = str(nusc.get_sample_data_path(sd_rec["token"]))
+    # if os.getcwd() in data_path:  # path from lyftdataset is absolute path
+    #     data_path = data_path.split(f"{os.getcwd()}/")[-1]  # relative path
+    sweep = {
+        "data_path": data_path,
+        "type": sensor_type,
+        "sample_data_token": sd_rec["token"],
+        "sensor2ego_translation": cs_record["translation"],
+        "sensor2ego_rotation": cs_record["rotation"],
+        "ego2global_translation": pose_record["translation"],
+        "ego2global_rotation": pose_record["rotation"],
+        "timestamp": sd_rec["timestamp"],
+    }
+    l2e_r_s = sweep["sensor2ego_rotation"]
+    l2e_t_s = sweep["sensor2ego_translation"]
+    e2g_r_s = sweep["ego2global_rotation"]
+    e2g_t_s = sweep["ego2global_translation"]
+
+    # obtain the RT from sensor to Top LiDAR
+    # sweep->ego->global->ego'->lidar
+    l2e_r_s_mat = Quaternion(l2e_r_s).rotation_matrix
+    e2g_r_s_mat = Quaternion(e2g_r_s).rotation_matrix
+    R = (l2e_r_s_mat.T @ e2g_r_s_mat.T) @ (
+        np.linalg.inv(e2g_r_mat).T @ np.linalg.inv(l2e_r_mat).T
+    )
+    T = (l2e_t_s @ e2g_r_s_mat.T + e2g_t_s) @ (
+        np.linalg.inv(e2g_r_mat).T @ np.linalg.inv(l2e_r_mat).T
+    )
+    T -= (
+        e2g_t @ (np.linalg.inv(e2g_r_mat).T @ np.linalg.inv(l2e_r_mat).T)
+        + l2e_t @ np.linalg.inv(l2e_r_mat).T
+    ).squeeze(0)
+    sweep["sensor2lidar_rotation"] = R.T  # points @ R.T + T
+    sweep["sensor2lidar_translation"] = T
+    return sweep
+
+def fill_test_infos(data_path, nusc, test_scenes, max_sweeps=10, with_camera=False):
+    test_nusc_infos = []
+    progress_bar = tqdm.tqdm(total=len(nusc.sample), desc="create_info", dynamic_ncols=True)
+
+    ref_chan = "LIDAR_TOP"
+    chan = "LIDAR_TOP"
+
+    for index, sample in enumerate(nusc.sample):
+        progress_bar.update()
+
+        ref_sd_token = sample["data"][ref_chan]
+        ref_sd_rec = nusc.get("sample_data", ref_sd_token)
+        ref_cs_rec = nusc.get("calibrated_sensor", ref_sd_rec["calibrated_sensor_token"])
+        ref_pose_rec = nusc.get("ego_pose", ref_sd_rec["ego_pose_token"])
+        ref_time = 1e-6 * ref_sd_rec["timestamp"]
+
+        ref_lidar_path, ref_boxes, _ = get_sample_data(nusc, ref_sd_token)
+
+        ref_cam_front_token = sample["data"]["CAM_FRONT"]
+        ref_cam_path, _, ref_cam_intrinsic = nusc.get_sample_data(ref_cam_front_token)
+
+        ref_from_car = transform_matrix(ref_cs_rec["translation"], Quaternion(ref_cs_rec["rotation"]), inverse=True)
+        car_from_global = transform_matrix(ref_pose_rec["translation"], Quaternion(ref_pose_rec["rotation"]), inverse=True)
+
+        info = {
+            "lidar_path": Path(ref_lidar_path).relative_to(data_path).__str__(),
+            "cam_front_path": Path(ref_cam_path).relative_to(data_path).__str__(),
+            "cam_intrinsic": ref_cam_intrinsic,
+            "token": sample["token"],
+            "sweeps": [],
+            "ref_from_car": ref_from_car,
+            "car_from_global": car_from_global,
+            "timestamp": ref_time,
+        }
+
+        # Add camera information if required
+        if with_camera:
+            # Add camera info processing here (similar to the original script)
+            pass
+
+        # Process sweeps
+        sample_data_token = sample["data"][chan]
+        curr_sd_rec = nusc.get("sample_data", sample_data_token)
+        sweeps = []
+        while len(sweeps) < max_sweeps - 1:
+            if curr_sd_rec["prev"] == "":
+                if len(sweeps) == 0:
+                    sweep = {
+                        "lidar_path": Path(ref_lidar_path).relative_to(data_path).__str__(),
+                        "sample_data_token": curr_sd_rec["token"],
+                        "transform_matrix": None,
+                        "time_lag": curr_sd_rec["timestamp"] * 0,
+                    }
+                    sweeps.append(sweep)
+                else:
+                    sweeps.append(sweeps[-1])
+            else:
+                curr_sd_rec = nusc.get("sample_data", curr_sd_rec["prev"])
+                # Process sweep (similar to the original script)
+                # ...
+
+        info["sweeps"] = sweeps
+
+        if sample["scene_token"] in test_scenes:
+            test_nusc_infos.append(info)
+
+    progress_bar.close()
+    return test_nusc_infos
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_root", required=True, help="Path to the nuScenes dataset.")
+    parser.add_argument("--output_root", required=True, help="Output path where processed information located.")
+    parser.add_argument("--max_sweeps", default=10, type=int, help="Max number of sweeps. Default: 10.")
+    parser.add_argument("--with_camera", action="store_true", default=False, help="Whether use camera or not.")
+    config = parser.parse_args()
+
+    print(f"Loading nuScenes tables for version v1.0-test...")
+    nusc_test = NuScenes(version="v1.0-test", dataroot=config.dataset_root, verbose=False)
+    available_scenes_test = get_available_scenes(nusc_test)
+    available_scene_names_test = [s["name"] for s in available_scenes_test]
+    print("total scene num:", len(nusc_test.scene))
+    print("exist scene num:", len(available_scenes_test))
+
+    test_scenes = splits.test
+    test_scenes = set([available_scenes_test[available_scene_names_test.index(s)]["token"] for s in test_scenes])
+
+    print(f"Filling test information...")
+    test_nusc_infos = fill_test_infos(config.dataset_root, nusc_test, test_scenes, max_sweeps=config.max_sweeps, with_camera=config.with_camera)
+
+    print(f"Saving nuScenes test information...")
+    os.makedirs(os.path.join(config.output_root, "info"), exist_ok=True)
+    print(f"test sample: {len(test_nusc_infos)}")
+    with open(os.path.join(config.output_root, "info", f"nuscenes_infos_{config.max_sweeps}sweeps_test.pkl"), "wb") as f:
+        pickle.dump(test_nusc_infos, f)
